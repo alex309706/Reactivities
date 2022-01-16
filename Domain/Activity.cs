3a67e5537dcc7fbb033b2d7d1515cc123a222dee
Alexandr Rahmankulov <Alexandr.Rahmankulov@intravision.ru>
2022-01-16 20:03:58 +0300

    End of section 14

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Domain
 {
@@ -8,9 +9,11 @@ namespace Domain
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
-
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        
+        public bool IsCancelled { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
     }
 }
--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -13,5 +13,25 @@ namespace Persistance
 
         public DbSet<Activity> Activities { get; set; }
         
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>()
+                .HasKey(x=> new {x.AppUserId,x.ActivityId });
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(x => x.AppUser)
+                .WithMany(x => x.Activities)
+                .HasForeignKey(x => x.AppUserId);
+            
+            builder.Entity<ActivityAttendee>()
+                .HasOne(x => x.Activity)
+                .WithMany(x => x.Attendees)
+                .HasForeignKey(x => x.ActivityId);
+        }
+
     }
 }
