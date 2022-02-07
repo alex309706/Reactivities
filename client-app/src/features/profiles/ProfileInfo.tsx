ea6eaef450c5d52d0ae7cc6df863809faa80da94
Alexandr Rahmankulov <Alexandr.Rahmankulov@intravision.ru>
2022-02-07 22:59:50 +0300

    Added component to view/update profile name and bio

--- client-app/src/features/profiles/ProfileInfo.tsx
+++ client-app/src/features/profiles/ProfileInfo.tsx
@@ -1,6 +1,6 @@
-﻿import React from "react";
+﻿import React, {useState} from "react";
 import {observer} from "mobx-react-lite";
-import {Grid, Header, Icon, Item, Tab} from "semantic-ui-react";
+import {Button, Form, Grid, Header, Icon, Input, Item, Tab, TextArea} from "semantic-ui-react";
 import {Profile} from "../../app/models/profile";
 import {useStore} from "../../app/stores/store";
 
@@ -11,22 +11,50 @@ interface Props {
 export default observer (function ProfileInfo ({profile}:Props){
     const {profileStore} = useStore();
     const {isCurrentUser} = profileStore;
+    const [editMode, setEditMode] = useState(false);
+    const [currentName, setCurrentName] = useState(profile.displayName);
     
     return(
         <Tab.Pane>
-            
-            <Grid>
+            <Grid width={15}>
+                <Grid.Column>
+                    <Icon name='user' size='big' />
+                </Grid.Column>
                 <Grid.Column width={12}>
-                    <Item.Group>
-                        <Item>
-                            <Item.Content verticalAlign="middle" >
-                                <Icon name='user' size='big' />
-                                <h1>About {profile.displayName}</h1>
-                            </Item.Content>
-                        </Item>
-                    </Item.Group>
+                    <h1>About {profile.displayName}</h1>
+                </Grid.Column>
+                <Grid.Column width={3} textAlign='right'>
+                    <Button
+                        basic
+                        onClick={ e => setEditMode(!editMode)}
+                    >{!editMode ? 'Edit Profile' : 'Cancel'}</Button>
                 </Grid.Column>
             </Grid>
+            {editMode &&
+            <Form>
+                <Grid>
+                    <Grid.Column width={16}>
+                        <Input
+                            type="text"
+                            fluid
+                            value = {currentName} 
+                            onChange={e => setCurrentName(e.target.value) }
+                        />
+                    </Grid.Column>
+
+                    <Grid.Column width={16}>
+                        <TextArea />
+                    </Grid.Column>
+
+                    <Grid.Column width={16} textAlign='right'>
+                        <Button color='green'
+                        
+                       >
+                            Update 
+                        </Button>
+                    </Grid.Column>
+                </Grid>
+            </Form>}
            
         </Tab.Pane>
     )
